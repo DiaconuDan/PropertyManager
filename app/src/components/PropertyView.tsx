6797bcb04c10fccb2f74106e7fd086c2467a47c3
Diaconu Dan <diaconudann@gmail.com>
2020-10-31 22:49:01 +0200

    Added query param filter. Small cleanup

--- app/src/components/PropertyView.tsx
+++ app/src/components/PropertyView.tsx
@@ -1,12 +1,11 @@
-import React, { FunctionComponent, useState,useEffect } from "react";
+import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import "react-notifications/lib/notifications.css";
 import ClipLoader from "react-spinners/ClipLoader";
 import { NotificationContainer } from "react-notifications";
 import useRequest from "../hooks/request/useRequest";
 import useQueryString from "../hooks/query/useQueryString";
-import useQueryApplicants from "../hooks/query/useQueryApplicants";
-import { Applicant, TApplicantList } from "./types";
+import { Applicant } from "./types";
 
 const Wrapper = styled.div`
   left: 0;
@@ -23,26 +22,16 @@ const PropertyView: FunctionComponent = () => {
     "https://5f9d70d76dc8300016d57b8a.mockapi.io/applicants"
   );
   const [query, onSetValue] = useQueryString("search", "");
-  // const [queriedApplicants] = useQueryApplicants(applicants, query);
 
-  // const [queriedApplicants, setQueriedApplicants] = useState<TApplicantList>([]);
- 
-  const searchedApplicants = applicants.length > 0 && applicants.filter((applicant:Applicant) => {
-    return (
-      applicant.firstName.toLowerCase().includes(query.toLowerCase()) ||
-      applicant.lastName.toLowerCase().includes(query.toLowerCase()) ||
-      applicant.email.toLowerCase().includes(query.toLowerCase())
-    );
-  });
-
-  console.log("searched app", searchedApplicants);
-
-  if (query) {
-    console.warn("avem query");
-  } else {
-    console.warn("nu avem query");
-  }
-
+  const queriedApplicants = query
+    ? applicants?.filter((applicant: Applicant) => {
+        return (
+          applicant.firstName.toLowerCase().includes(query.toLowerCase()) ||
+          applicant.lastName.toLowerCase().includes(query.toLowerCase()) ||
+          applicant.email.toLowerCase().includes(query.toLowerCase())
+        );
+      })
+    : applicants;
 
   return (
     <Wrapper>
@@ -53,8 +42,8 @@ const PropertyView: FunctionComponent = () => {
         onChange={(e) => onSetValue(e.target.value)}
       ></input>
       <ClipLoader size={50} color={"#36D7B7"} loading={isLoading} />
-      {!isLoading && !isError && applicants.length > 0 && (
-        <div> {applicants[0].firstName} </div>
+      {!isLoading && !isError && queriedApplicants && (
+        <div> {queriedApplicants[0]?.firstName} </div>
       )}
     </Wrapper>
   );
